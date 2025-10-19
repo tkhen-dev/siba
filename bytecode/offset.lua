b67638f54083eaa0cfe9f63b6c9da8c31099f8e4
tkhen-dev <kphat0464@gmail.com>
2025-10-19 21:53:34 +0700

    Update offset.lua

--- bytecode/offset.lua
+++ bytecode/offset.lua
@@ -1,4 +1,4 @@
-local function equal(t1,t2)
+local function equal(t1, t2)
     if type(t1) ~= type(t2) then
         return false
     end
@@ -7,60 +7,62 @@ local function equal(t1,t2)
         return t1 == t2
     end
 
-    for i,v in pairs(t1) do
+    for i, v in pairs(t1) do
         if type(v) == "table" then
-            if type(t2[i]) == "table" then
-                if not equal(v, t2[i]) then
-                    return false
-                end
-            else
-                return false 
+            if not equal(v, t2[i]) then
+                return false
             end
+        elseif v ~= t2[i] then
+            return false
         end
+    end
 
-        if v ~= t2[i] then return false end
+    for i in pairs(t2) do
+        if t1[i] == nil then
+            return false
+        end
     end
 
     return true
 end
 
-local function find(t,val)
-    for i,v in pairs(t) do
-        if equal(v,val) then return i end
+local function find(t, val)
+    for i, v in pairs(t) do
+        if equal(v, val) then
+            return i
+        end
     end
 end
 
 local function bfs(chunk)
     local visited = {}
     local queue = {}
-
     local order = {}
 
-    table.insert(visited, chunk)
+    visited[chunk] = true
     table.insert(queue, chunk)
 
     while #queue > 0 do
-        local element = table.remove(queue,1)
+        local element = table.remove(queue, 1)
         table.insert(order, element)
-    
-        for i,v in pairs(element.protos) do
-            if not find(visited, v) then
-                table.insert(visited, v)
-                table.insert(queue,v)
+
+        for _, v in pairs(element.protos or {}) do
+            if not visited[v] then
+                visited[v] = true
+                table.insert(queue, v)
             end
         end
     end
-    
+
     return order
 end
 
 local function offset(chunk)
     local order = bfs(chunk)
-
     local instr_offset = 1
 
-    for i,v in pairs(order) do
-        v["instr_offset"] = instr_offset
+    for _, v in pairs(order) do
+        v.instr_offset = instr_offset
         local count = #v.instructions
         instr_offset = instr_offset + count
     end
